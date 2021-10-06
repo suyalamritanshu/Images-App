c1e4c80b3f50b39ea5b9255830d3c6d410559788
Siddharth Singh <siddhartsingh2002@gmail.com>
2021-10-06 20:45:14 +0530

    Add SplashScreen

---
+++ ImagesApp/app/src/main/java/com/example/imagesapp/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.imagesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        int colorCodeDark = Color.parseColor("#303030");
+        window.setStatusBarColor(colorCodeDark);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+
+    }
+}
